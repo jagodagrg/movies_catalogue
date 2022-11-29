e307ca2d0c59dbfd71b6dc3c8076ca009cfe1ae9
Jagoda Giurg <jagoda.giurg@gmail.com>
2022-11-29 17:41:35 +0000

    Update first stage of the project

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 import requests
-import random
 
 app = Flask(__name__)
 
 
 def import_data():
-    endpoint = "https://api.themoviedb.org/3/trending/movie/day?api_key=89434bbb2ca15a8a22e3314335efc93c"
-    api_token = "eyJhdWQiOiI4OTQzNGJiYjJjYTE1YThhMjJlMzMxNDMzNWVmYzkzYyIsInN1YiI6IjYzNzc3NDBjMTU2Y2M3MDA3NjMxMGQyMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ"
+    endpoint = "https://api.themoviedb.org/3/trending/movie/day"
+    api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4OTQzNGJiYjJjYTE1YThhMjJlMzMxNDMzNWVmYzkzYyIsInN1YiI6IjYzNzc3NDBjMTU2Y2M3MDA3NjMxMGQyMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RXUlsdR3Qsx-y5PlKI8kocGgtqrR1XDvSBCby3lrx2w"
     headers = {
         "Authorisation": f'Bearer {api_token}'
     }
