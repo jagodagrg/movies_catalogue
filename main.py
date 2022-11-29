fb2233fa9f3646862b72b40532df2d0dd08513e7
Jagoda Giurg <jagoda.giurg@gmail.com>
2022-11-29 18:21:50 +0000

    Add screenshot

--- main.py
+++ main.py
@@ -5,20 +5,20 @@ app = Flask(__name__)
 
 
 def import_data():
-    endpoint = "https://api.themoviedb.org/3/trending/movie/day"
+    endpoint = "https://api.themoviedb.org/3/trending/movie/day?api_key=89434bbb2ca15a8a22e3314335efc93c"
     api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4OTQzNGJiYjJjYTE1YThhMjJlMzMxNDMzNWVmYzkzYyIsInN1YiI6IjYzNzc3NDBjMTU2Y2M3MDA3NjMxMGQyMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RXUlsdR3Qsx-y5PlKI8kocGgtqrR1XDvSBCby3lrx2w"
     headers = {
         "Authorisation": f'Bearer {api_token}'
     }
     response = requests.get(endpoint, headers=headers)
-    all_movies = response.json()['results']
+    all_movies = response.json()
     return all_movies
 
 
 @ app.route('/')
 def homepage():
     movies = []
-    all_movies = import_data()
+    all_movies = import_data()['results']
     for i in range(8):
         movies.append(all_movies[i])
     return render_template("homepage.html", movies=movies)
